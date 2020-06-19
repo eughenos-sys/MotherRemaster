16f5e60e0170554214965f559b3c935d2f1d1ad1
Luca Calabrese <Luca Calabrese@DESKTOP-0Q24JDI>
2020-06-19 16:35:10 +0200

    user class managed

--- Assets/Scripts/Data/Behaviours/UserBehaviour.cs
+++ Assets/Scripts/Data/Behaviours/UserBehaviour.cs
@@ -10,5 +10,35 @@ public class UserBehaviour
     public string password;
     public int rank=100;
     public int credit=100;
+    public string className {get{
+        if(this.rank<100){
+            return "Fool Lamer";
+        }
+        if(this.rank<200){
+            return "Lamer";
+        }
+        if(this.rank<500){
+            return "Scripter";
+        }
+        if(this.rank<1000){
+            return "Coder";
+        }
+        if(this.rank<1500){
+            return "Code Hacker";
+        }
+        if(this.rank<2000){
+            return "Cracker";
+        }
+        if(this.rank<2500){
+            return "Master Cracker";
+        }
+        if(this.rank<3000){
+            return "Hacker";
+        }
+        if(this.rank<3500){
+            return "Master Hacker";
+        }
+        return string.Empty;
+    }}
     public string mail {get{return user + "@mother";}}
 }
