dd833e51a505bdec98777927c94650a32769c49f
Luca Calabrese <Luca Calabrese@DESKTOP-0Q24JDI>
2020-06-20 17:05:57 +0200

    rootbreaker works

--- Assets/RootBreakerBehaviour.cs
+++ Assets/RootBreakerBehaviour.cs
@@ -7,26 +7,34 @@ public class RootBreakerBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     public TextMeshProUGUI rootbreaker;
-    private float time = 0.0f;
-    public float interpolationPeriod = 1f;
+    private float charTime = 0.0f;
+    public float interpolationPeriod = 3.0f;
+    private int charactersFound=0;
+    private int passwordSize=12;
     void Start()
     {
         Debug.Log("ciao");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rootbreaker.text.Length<12){
-        for(int i=0;i<12;i++)
-        {
-            time=0.0f;
-            while(time<interpolationPeriod){
-                time+=Time.deltaTime;
-                rootbreaker.text=rootbreaker.text.Substring(0,i);
-                rootbreaker.text+=(char)Random.Range(33,126);
-            }
-}
-        }
+         if(charactersFound <passwordSize){
+                 charTime+=Time.deltaTime;
+        //         var txtLenght=rootbreaker.text.Length;
+                if (charTime <  interpolationPeriod)
+                {
+                    
+                    rootbreaker.text = rootbreaker.text.Substring(0,charactersFound);
+                    rootbreaker.text += (char)Random.Range(33, 126);
+                    
+                }
+               else{
+                   
+                       charTime=0.0f;
+                   charactersFound++;
+               }
+         }
     }
 }
