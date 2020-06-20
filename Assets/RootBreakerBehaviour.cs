ccf20743f52ee98ebb68268b9a086c08f178825e
Luca Calabrese <Luca Calabrese@DESKTOP-0Q24JDI>
2020-06-20 16:12:21 +0200

    root

--- Assets/RootBreakerBehaviour.cs
+++ Assets/RootBreakerBehaviour.cs
@@ -6,19 +6,27 @@ using TMPro;
 public class RootBreakerBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
-    TextMeshProUGUI rootbreaker;
+    public TextMeshProUGUI rootbreaker;
+    private float time = 0.0f;
+ public float interpolationPeriod = 1f;
     void Start()
     {
-        
+        Debug.Log("ciao");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        while (Time.deltaTime < Time.deltaTime+5f){
-            //genero caratteri e alla fine del ciclo text += carattere.
-            //when numero di caratteri in textbox = 12 end
+        if(rootbreaker.text.Length<12){
+        for(int i=0;i<12;i++)
+        {
+            time=0.0f;
+            while(time<interpolationPeriod){
+                time+=Time.deltaTime;
+                rootbreaker.text=rootbreaker.text.Substring(0,i);
+                rootbreaker.text+=(char)Random.Range(33,126);
+            }
+}
         }
     }
 }
